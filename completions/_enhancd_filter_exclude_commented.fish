d9c096e0a7f3012ee44128977c0b6f0b6e162777
Jiho Lee <optional.int@kakao.com>
2023-01-09 14:21:24 +0900

    feat: initial commit

---
+++ completions/_enhancd_filter_exclude_commented.fish
@@ -0,0 +1,1 @@
+/home/dps0340/.zplugin/plugins/b4b4r07---enhancd/functions/_enhancd_filter_exclude_commented.fish
---
+++ completions/_enhancd_ltsv_open.fish
@@ -0,0 +1,1 @@
+/home/dps0340/.zplugin/plugins/b4b4r07---enhancd/functions/_enhancd_ltsv_open.fish
